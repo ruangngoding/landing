0a728a37a20e0a3298bec4176d428074c323bee3
Jehudavd <jehudavd@gmail.com>
2023-11-14 18:43:03 +0800

    update hamburger menu

--- script.js
+++ script.js
@@ -27,3 +27,28 @@ document.addEventListener("DOMContentLoaded", function () {
     // Menampilkan profil pertama saat halaman dimuat
     showProfile(currentProfileIndex);
 });
+
+// Update the existing JavaScript code
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const navList = document.querySelector(".nav-list");
+
+    hamburgerMenu.addEventListener("click", function () {
+        navList.classList.toggle("show");
+    });
+
+    // Close the navigation if a menu item is clicked
+    document.querySelectorAll(".nav-list a").forEach(function (item) {
+        item.addEventListener("click", function () {
+            navList.classList.remove("show");
+        });
+    });
+
+    // Close the navigation if "Masuk" or "Daftar" button is clicked
+    document.querySelectorAll(".mobile-login, .mobile-register").forEach(function (item) {
+        item.addEventListener("click", function () {
+            navList.classList.remove("show");
+        });
+    });
+});
